783e3ce542e72e335ec8e92d0aa7efcb136e5a03
RileyWildbots3865 <nyarrobotics3865@gmail.com>
2023-02-21 17:27:14 -0500

    Added George

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.subDriveTrain;
+import frc.robot.subsystems.subPneumatics;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
@@ -20,16 +21,16 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 
 public class RobotContainer {
-  private final XboxController m_driverOne = new XboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController m_driverOne = new CommandXboxController(OperatorConstants.kDriverControllerPort);
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final subDriveTrain driveTrain = new subDriveTrain();
-  JoystickButton driverOne_A = new JoystickButton(m_driverOne , Button.kA.value);  
-  
+  private final subDriveTrain driveTrain = new subDriveTrain(); 
+  private final subPneumatics george = new subPneumatics();
 
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
@@ -45,7 +46,7 @@ public class RobotContainer {
 
 
     driveTrain.setDefaultCommand(Commands.run(() -> driveTrain.drive(-m_driverOne.getLeftY(), -m_driverOne.getRightY()), driveTrain));
-    //driverOne_A.onTrue(new InstantCommand() -> lift.toggle() );
+    m_driverOne.a().onTrue(new InstantCommand(() -> george.liftSolenoid.toggle()));
   }
 
   /**
