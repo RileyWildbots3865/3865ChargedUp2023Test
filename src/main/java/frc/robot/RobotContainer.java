1e28d2265bdd889544660af8a87258bb5057fa8e
RileyWildbots3865 <nyarrobotics3865@gmail.com>
2023-02-21 16:50:16 -0500

    removed void in RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -33,7 +33,7 @@ public class RobotContainer {
 
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public void RobotContainer() {
+  public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
   }
