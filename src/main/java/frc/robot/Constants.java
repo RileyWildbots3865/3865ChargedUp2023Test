85f3ce05f699987f8d4e8cce3caa670b9b0169b2
RileyWildbots3865 <nyarrobotics3865@gmail.com>
2023-03-11 11:41:05 -0500

    reverted again

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,11 +16,6 @@ public final class Constants {
     public static final boolean frontRightInverted = true;
     public static final int backRight = 3;
     public static final boolean backRightInverted = true;
-    public static final int clawMotor = 14;
-    public static final boolean clawMotorInverted = true;
-    
-      
-    }
     public static final double speedFactor = 0.5;
 
   
@@ -32,3 +27,4 @@ public final class Constants {
 
     
   }
+}
