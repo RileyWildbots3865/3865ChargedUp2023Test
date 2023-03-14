052da6d9e9f924429a2546444f31aa8ef4e315a0
RileyWildbots3865 <nyarrobotics3865@gmail.com>
2023-03-14 18:36:09 -0400

    Motor

--- src/main/java/frc/robot/subsystems/subRandomStuff.java
+++ src/main/java/frc/robot/subsystems/subRandomStuff.java
@@ -1,19 +1,21 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-/* 
+
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import frc.robot.Constants.RandomStuff;
 
 public class subRandomStuff extends SubsystemBase {
   public CANSparkMax clawMotor;
   public subRandomStuff() {
-    frontLeft = new CANSparkMax(RandomStuff.clawMotor, MotorType.kBrushless);
+    clawMotor = new CANSparkMax(RandomStuff.clawMotor, MotorType.kBrushless);
     
-    frontLeft.restoreFactoryDefaults();
+    clawMotor.restoreFactoryDefaults();
   }
 
   @Override
@@ -21,4 +23,3 @@ public class subRandomStuff extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 }
-*/
